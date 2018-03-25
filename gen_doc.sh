d9089f0c8882c49ad02cc7204a6b9c9bba248a46
mironal <miro.gunp@gmail.com>
2018-03-25 13:27:44 +0900

    add API document.

---
+++ gen_doc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+jazzy \
+  --clean \
+  --author mironal \
+  --author_url https://github.com/mironal \
+  --github_url https://github.com/mironal/ShortHandAlert \
+  --module ShortHandAlert \
+  --output docs
